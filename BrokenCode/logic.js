dbfd29d0dac37f7a09df1c354cb96bb3caa0f796
Samantha <54869367+SambooG@users.noreply.github.com>
2019-09-25 21:15:12 -0400

    Add files via upload

---
+++ BrokenCode/logic.js
@@ -0,0 +1,40 @@
+//when the fact button is clicked
+$("#factButton").on("click", function() {
+	//fact is generated
+	var number = Math.floor((Math.random() * booFacts.length));
+	//fact is displayed
+	$("#factText").text(booFacts[number])
+})
+//holds factText
+var booFacts = ["Boo is a pomeranian", "Boo's best friend is another pomeranian named Buddy", "Boo the Pomeranian was born on March 16, making him a Pisces", "Boo's favourite food is grass, Boo has released two books"]
+
+// When the textPink button is pressed...
+$("#textPink").on("click", function() {
+	// Change funText to pink.
+	$("#funText").css("color", "pink")
+})
+
+// When the textOrange button is pressed...
+$("#textOrange").on("click", function() {
+	// Change funText to orange.
+	$("#funText").css("color", "orange")
+})
+
+// When the textGreen button is pressed...
+$("#textGreen").on("click", function() {
+	// Change funText to green.
+	$("#funText").css("color", "green")
+})
+
+
+// When the boxGrow button is clicked... 
+$("#boxGrow").on("click", function() {
+	// Increase the size of the box.
+	$("#box").animate({height:"+=35px", width:"+=35px"}, "fast");
+})
+
+// When the boxShrink button is clicked...
+$("#boxShrink").on("click", function() {
+	// Decrease the size of the box.
+	$("#box").animate({height:"-=35px", width:"-=35px"}, "fast");
+})
